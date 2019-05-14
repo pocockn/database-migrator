428c44ef7273a34c3eeadb3bc4a05d3e189e035e
pocockn <pocockn@hotmail.co.uk>
2019-05-14 21:06:20 +0100

    Remove dist on gorelease

--- release.sh
+++ release.sh
@@ -3,9 +3,9 @@
 echo "Releasing version $1"
 
 if git rev-parse "v$1" >/dev/null 2>&1; then
-    goreleaser
+    goreleaser --rm-dist
 else
     git tag -a v$1 -m "Release number: $1"
     git push origin v$1
-    goreleaser
+    goreleaser --rm-dist
 fi
